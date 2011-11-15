756ae0ced6c6c1067b76c0bbfd16eb7a75c7efd9
Hiro_i <sasakawa@ist.hokudai.ac.jp>
2011-11-15 18:12:05 +0900

    under construction

--- QuadTree.py
+++ QuadTree.py
@@ -7,6 +7,17 @@ class QuadTree:
         self.NE = None
         self.SW = None
         self.SE = None
+        self.depth = 0
+        self.node_num = 1
+
+    def makeQT(self, poitlist):
+
+    def searchNode()
 
 
-    def makeQT(self)
+if __name__ == '__main__':
+    points = [(1,2), (4,4), (2,5), (9,8)]
+    quad_tree = QuadTree(0,0)
+    
+    quad_tree.makeQT(points)
+    
