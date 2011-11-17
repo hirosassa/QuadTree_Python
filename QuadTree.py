8310a58f37f0adbff2bbcc14c8c717526ff27695
SassaHero <hiro.sassa@gmail.com>
2011-11-17 09:52:15 +0900

    class architecture fixed

--- QuadTree.py
+++ QuadTree.py
@@ -1,47 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+class QTNode:
+    def __init__(self, point, label):
+        self.label = label
+        self.x = point[0]
+        self.y = point[1]
+        self.NW = None
+        self.NE = None
+        self.SW = None
+        self.SE = None
+        self.Parent = None
             
 class QuadTree:
-    class Node:
-        def __init__(self, point, label):
-            self.label = label
-            self.x = point[0]
-            self.y = point[1]
-            self.NW = None
-            self.NE = None
-            self.SW = None
-            self.SE = None
-
-        def insert(self, newPoint, label):
-                if self.x <= newPoint[0] and self.y <= newPoint[1] :  # NE
-                    if self.NE is None:
-                        self.NE = QuadTree.Node(newPoint, label)
-                    else : self.NE = insert(newPoint, label)
-                elif self.x <= newPoint[0] and self.y > newPoint[1] : # SE
-                    if self.SE is None:
-                        self.SE = QuadTree.Node(newPoint, label)
-                    else : self.SE = insert(newPoint, label)
-                elif self.x > newPoint[0] and self.y <= newPoint[1] : # NW
-                    if self.NW is None:
-                        self.NW = QuadTree.Node(newPoint, label)
-                    else : self.NW = insert(newPoint, label)
-                else :                                                # SW
-                    if self.SW is None:
-                        self.SW= QuadTree.Node(newPoint, label)
-                    else : self.SW = insert(newPoint, label)
-
-
+    """
+    Simple point quad tree implementaion.
+    This quad tree supports insert, and searchNode operations.
+    """
     def __init__(self):
         self.root = None
         self.nodeNum = 0
             
     def insertNode(self, newPoint):
+        """Insert point into this quad tree."""
+        new_node = QTNode(newPoint, self.nodeNum)
         if self.root is None :
-            self.root = QuadTree.Node(newPoint, self.nodeNum)
+            self.root = new_node
         else :
-            self.root.insert(newPoint, self.nodeNum)
+            node = self.root
+            while True :
+                if newPoint[0] >= node.x and newPoint[1] >= node.y :  # NE region
+                    if node.NE is None :
+                        node.NE = new_node
+                        new_node.Parent = node
+                        break
+                    node = node.NE
+                elif newPoint[0] >= node.x and newPoint[1] < node.y : # SE region
+                    if node.SE is None :
+                        node.SE = new_node
+                        new_node.Parent = node
+                        break
+                    node = node.SE
+                elif newPoint[0] < node.x and newPoint[1] >= node.y : # NW region
+                    if node.NW is None :
+                        node.NW = new_node
+                        new_node.Parent = node
+                        break
+                    node = node.NW
+                else :                                                # SW region
+                    if node.SW is None :
+                        node.SW = new_node
+                        new_node.Parent = node
+                        break
+                    node = node.SW
         self.nodeNum += 1
+        return new_node
+                    
+    
+    def searchNode(self, point):
+        """
+        Return the node for coordinates if the node is in the quad tree,
+        or None otherwise.
+        """
+        node = self.root
+        while node is not None :
+            if point[0] == node.x and point[1] == node.y :   # Find!!
+                return node
+            elif point[0] >= node.x and point[1] >= node.y : # Go to NE region
+                node = node.NE
+            elif point[0] >= node.x and point[1] < node.y :  # Go to SE region
+                node = node.SE
+            elif point[0] < node.x and point[1] >= node.y :  # Go to NW region
+                node = node.NW                                    
+            else :                                           # Go to SW region
+                node = node.SW
+
+
+
 
 
     #def searchNode(self, point):
